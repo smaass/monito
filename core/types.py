088c1ea69bfbb0ff47594fd561ca3233c7d2d247
Sergio Maass <sergio.maass@gmail.com>
2016-02-20 01:29:25 -0300

    Types with tests of consistency

--- core/types.py
+++ core/types.py
@@ -15,11 +15,17 @@ class Type(object):
     def is_consistent_with_list(self, inner_type):
         return False
 
+    def is_consistent_with_fun_type(self, fun_type):
+        return False
+
 
 class NumType(Type):
 
     def is_consistent_with(self, another_type):
         return another_type.is_consistent_with_num()
+
+    def is_consistent_with_num(self):
+        return True
 
 
 class BoolType(Type):
@@ -27,17 +33,23 @@ class BoolType(Type):
     def is_consistent_with(self, another_type):
         return another_type.is_consistent_with_bool()
 
+    def is_consistent_with_bool(self):
+        return True
+
 
 class StringType(Type):
 
     def is_consistent_with(self, another_type):
         return another_type.is_consistent_with_string()
 
+    def is_consistent_with_string(self):
+        return True
+
 
 class ListType(Type):
 
-    def list_type(self, inner_type):
-        self.inner_type
+    def __init__(self, inner_type):
+        self.inner_type = inner_type
 
     def is_consistent_with(self, another_type):
         return another_type.is_consistent_with_list(self.inner_type)
@@ -46,19 +58,55 @@ class ListType(Type):
         return self.inner_type.is_consistent_with(inner_type)
 
 
+class FunType(Type):
+
+    def __init__(self, arg_types, return_type):
+        self.arg_types = arg_types
+        self.return_type = return_type
+
+    def is_consistent_with(self, another_type):
+        return another_type.is_consistent_with_fun_type(self)
+
+    def is_consistent_with_fun_type(self, fun_type):
+
+        if len(self.arg_types) != len(fun_type.arg_types):
+            return False
+
+        for i in range(0, len(fun_type.arg_types)):
+            if not self.arg_types[i].is_consistent_with(fun_type.arg_types[i]):
+                return False
+
+        return self.return_type.is_consistent_with(fun_type.return_type)
+
+
 class DynamicType(Type):
 
     def __init__(self, last_type=None):
         self.last_type = last_type
 
     def is_consistent_with(self, another_type):
+        """
+        Mutates this DynamicType assigning another_type
+        to self.last_type after the first consistency check.
+        This is used to ensure that the consistency relation is
+        not transitive.
+        """
         if self.last_type is not None:
-            self.last_type.is_consistent_with(another_type)
+            return self.last_type.is_consistent_with(another_type)
+        self.last_type = another_type
         return True
 
+    def is_consistent_with_num(self):
+        return self.is_consistent_with(NumType())
 
-class FunType(Type):
+    def is_consistent_with_bool(self):
+        return self.is_consistent_with(BoolType())
 
-    def __init__(self, arg_types, return_type):
-        self.arg_types = arg_types
-        self.return_type = return_type
+    def is_consistent_with_string(self):
+        return self.is_consistent_with(StringType())
+
+    def is_consistent_with_list(self, inner_type):
+        return self.is_consistent_with(ListType(inner_type))
+
+    def is_consistent_with_fun_type(self, fun_type):
+        return self.is_consistent_with(fun_type)
---
+++ tests/test_types.py
@@ -0,0 +1,87 @@
+import unittest
+from core.types import *
+
+
+class TypesTestCase(unittest.TestCase):
+
+    def test_num_consistency(self):
+
+        self.assertTrue(NumType().is_consistent_with(NumType()))
+        self.assertTrue(NumType().is_consistent_with(DynamicType()))
+        self.assertFalse(NumType().is_consistent_with(BoolType()))
+        self.assertFalse(NumType().is_consistent_with(StringType()))
+        self.assertFalse(NumType().is_consistent_with(ListType(NumType())))
+        self.assertFalse(NumType().is_consistent_with(FunType([], NumType())))
+
+    def test_bool_consistency(self):
+
+        self.assertTrue(BoolType().is_consistent_with(BoolType()))
+        self.assertTrue(BoolType().is_consistent_with(DynamicType()))
+        self.assertFalse(BoolType().is_consistent_with(NumType()))
+        self.assertFalse(BoolType().is_consistent_with(StringType()))
+        self.assertFalse(BoolType().is_consistent_with(ListType(BoolType())))
+        self.assertFalse(BoolType().is_consistent_with(FunType([], BoolType())))
+
+    def test_string_consistency(self):
+
+        self.assertTrue(StringType().is_consistent_with(StringType()))
+        self.assertTrue(StringType().is_consistent_with(DynamicType()))
+        self.assertFalse(StringType().is_consistent_with(BoolType()))
+        self.assertFalse(StringType().is_consistent_with(NumType()))
+        self.assertFalse(StringType().is_consistent_with(ListType(StringType())))
+        self.assertFalse(StringType().is_consistent_with(FunType([], StringType())))
+
+    def test_list_consistency(self):
+
+        list_of_nums = ListType(NumType())
+        list_of_strings = ListType(StringType())
+        list_of_dynamic = ListType(DynamicType())
+
+        self.assertTrue(list_of_nums.is_consistent_with(ListType(NumType())))
+        self.assertTrue(list_of_nums.is_consistent_with(DynamicType()))
+        self.assertTrue(list_of_nums.is_consistent_with(list_of_dynamic))
+        self.assertFalse(list_of_nums.is_consistent_with(list_of_strings))
+        self.assertFalse(list_of_nums.is_consistent_with(NumType()))
+
+    def test_fun_consistency(self):
+
+        self.assertTrue(
+            FunType([], StringType()).is_consistent_with(FunType([], StringType()))
+        )
+        self.assertFalse(
+            FunType([], StringType()).is_consistent_with(FunType([], NumType()))
+        )
+
+        fun1 = FunType([BoolType(), NumType()], NumType())
+        fun2 = FunType([BoolType(), NumType()], NumType())
+        self.assertTrue(fun1.is_consistent_with(fun2))
+
+        fun2 = FunType([NumType(), BoolType()], NumType())
+        self.assertFalse(fun1.is_consistent_with(fun2))
+
+        fun3 = FunType([BoolType(), NumType()], StringType())
+        self.assertFalse(fun1.is_consistent_with(fun3))
+
+        fun4 = FunType([fun1], fun3)
+        fun5 = FunType([fun1], fun3)
+        fun6 = FunType([fun2], fun4)
+        self.assertTrue(fun4.is_consistent_with(fun5))
+        self.assertFalse(fun4.is_consistent_with(fun6))
+
+    def test_dynamic_consistency(self):
+
+        self.assertTrue(DynamicType().is_consistent_with(NumType()))
+        self.assertTrue(DynamicType().is_consistent_with(BoolType()))
+        self.assertTrue(DynamicType().is_consistent_with(StringType()))
+        self.assertTrue(DynamicType().is_consistent_with(ListType(StringType())))
+        self.assertTrue(DynamicType().is_consistent_with(FunType([], NumType())))
+
+        dynamic_type = DynamicType()
+        self.assertTrue(StringType().is_consistent_with(dynamic_type))
+        self.assertFalse(NumType().is_consistent_with(dynamic_type))
+        self.assertTrue(StringType().is_consistent_with(dynamic_type))
+
+        dynamic_type = DynamicType()
+        self.assertTrue(NumType().is_consistent_with(dynamic_type))
+        self.assertFalse(StringType().is_consistent_with(dynamic_type))
+        self.assertTrue(NumType().is_consistent_with(dynamic_type))
